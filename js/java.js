32f266ebd2bb8394b7a71808161b449a620d9245
regina16molina <molinaregina165@gmail.com>
2023-08-22 00:36:26 -0300

    correciones entrega proyecto final

--- js/java.js
+++ js/java.js
@@ -444,13 +444,27 @@ cards.forEach((card) => {
 // Cargar productos desde JSON
 async function cargarProductosDesdeJSON() {
     try {
-        const response = await fetch('productos.json'); // Cambia la ruta si es necesario
-        const productos = await response.json();
-        cargarProductos(productos);
+      const response = await fetch('productos.json'); // Cambia la ruta si es necesario
+      const productosData = await response.json();
+  
+      const productos = productosData.map(productoData => {
+        return new Producto(
+          productoData.id,
+          productoData.titulo,
+          productoData.imagen,
+          productoData.categoria,
+          productoData.precio
+        );
+      });
+  
+      productos.forEach(producto => {
+        // Resto del código para crear y agregar productos
+        // (puedes reutilizar el código que tenías para cargar productos)
+      });
     } catch (error) {
-        console.error('Error al cargar productos desde JSON:', error);
+      console.error('Error al cargar productos desde JSON:', error);
     }
-}
+  }
   
   // Llamada para cargar productos desde JSON
   cargarProductosDesdeJSON();
---
+++ js/producto.js
@@ -0,0 +1,13 @@
+
+class Producto {
+    constructor(id, titulo, imagen, categoria, precio) {
+      this.id = id;
+      this.titulo = titulo;
+      this.imagen = imagen;
+      this.categoria = categoria;
+      this.precio = precio;
+    }
+  }
+  
+  export default Producto;
+  
